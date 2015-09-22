a952be9ddf06bc0c5160a502faaca70f00bc2e85
Thomas Watson Steen <w@tson.dk>
2015-09-22 18:44:57 +0200

    Improve tests

--- test.js
+++ test.js
@@ -4,5 +4,7 @@ var MAX_SAFE_INTEGER = require('./');
 
 test(function (t) {
 	t.assert(typeof MAX_SAFE_INTEGER === 'number');
+	t.is(MAX_SAFE_INTEGER + 1, MAX_SAFE_INTEGER + 2);
+	t.not(MAX_SAFE_INTEGER - 1, MAX_SAFE_INTEGER - 2);
 	t.end();
 });
